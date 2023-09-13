62dc78e5f2c14d75576562640e4fa1b3ea690af1
Gerardo Quispe <gerardo.qantara@gmail.com>
2023-09-12 19:42:08 -0500

    Se realizo la practica 3

--- transform.js
+++ transform.js
@@ -2,7 +2,7 @@ const datos=[
     {
         id: 1,
         nomnbre: 'Juan',
-        habilidades: ['JavaCript', 'HTML', 'CSS'],
+        habilidades: ['JavaScript', 'HTML', 'CSS'],
         proyectos: [
             { id: 1, nombre: 'Proyecto 1'},
             { id: 2, nombre: 'Proyecto 2'}
@@ -10,7 +10,7 @@ const datos=[
     },
     {
         id: 2,
-        nomnbre: 'Maria',
+        nombre: 'Maria',
         habilidades: ['Python', 'SQL', 'Django'],
         proyectos: [
             { id: 3, nombre: 'Proyecto 3'},
@@ -26,6 +26,18 @@ const datos=[
             { id: 6, nombre: 'Proyecto 6'}
         ]
     }
-]
-const desarrolladoresJavascript=datos.filter()
+];
+const desarrolladoresJavascript=datos.filter(dato=>(dato.habilidades
+                                     .includes('JavaScript')
+    ))
+        console.log(desarrolladoresJavascript)
 
+const nombresProyectos=datos.map(dato=>(dato.proyectos
+                            .map(proyecto => proyecto.nombre))).flat();
+        console.log(nombresProyectos);
+
+  
+  
+  
+  
+  
